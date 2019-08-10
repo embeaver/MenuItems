def758390d0ef5b431bff3a82a80aa2a58343f4a
Erika Beaver <erikambeaver@gmail.com>
2019-08-10 16:16:28 -0400

    edit, delete, and new links on page

--- vagrant/Menu/project.py
+++ vagrant/Menu/project.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -15,8 +15,8 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-# @app.route('/')
-@app.route('/restaurants/<int:restaurant_id>/', methods=['GET', 'POST'])
+@app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
@@ -24,14 +24,32 @@ def restaurantMenu(restaurant_id):
 
 
 # route for newMenuItem function
-@app.route('/restaurants/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/',
+           methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return 'page to create new menu item'
+    if request.method == 'POST':
+        newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/',
+           methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return 'page to edit'
+    editedItem = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('editmenuitem.html', restaurant_id=restaurant_id,
+                               menu_id=menu_id, i=editedItem)
 
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
